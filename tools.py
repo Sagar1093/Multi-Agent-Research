3306385bd34fd2c8055a91111daa4c8d5c687cdd
Sagar <sagarn2404@gmail.com>
2026-08-15 21:08:22 +0530

    Multi Agent Pipeline Built

--- tools.py
+++ tools.py
@@ -11,15 +11,15 @@ tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 
 @tool
 def web_search(query:str) -> str:
-    """Search the web for recent and reliable information on a given topic.Returns Titles,URLs and snippets"""
+    """Search the web for recent and reliable information on a given topic.Returns Titles,URLs and summary"""
     results = tavily.search(query = query,max_results=5)
     out = []
 
     for r in results["results"]:
         title = r["title"]
         url = r["url"]
-        snippet = r["snippet"][:300]
-        out.append(f"Title: {title}\nURL: {url}\nSnippet: {snippet}\n")
+        summary = r["content"][:300]
+        out.append(f"Title: {title}\nURL: {url}\n Summary: {summary}\n")
 
     return "\n---\n".join(out)
 
@@ -31,8 +31,7 @@ def scrape_url(url:str)->str:
         soup = BeautifulSoup(resp.text, 'html.parser')
         for tag in soup(["script","style","nav","footer"]):
             tag.decompose()
-        return soup.get_text(separator=" ",strip=True)[:2000]
+        return soup.get_text(separator=" ",strip=True)[:1000]
     except Exception as e:
         return f"Error scraping URL: {str(e)}"
 
-print(scrape_url.invoke("https://www.comicbasics.com/spider-man-brand-new-day-just-dethroned-no-way-home-and-its-officially-the-biggest-spider-man-movie-ever/"))
